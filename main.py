1718b74da5c0ef9bb31df934b001bb5b2ee04aa4
Lukasz Prokulski <prokulski@gmail.com>
2021-09-11 17:27:16 +0200

    obsługa wielu kont

--- main.py
+++ main.py
@@ -9,24 +9,46 @@ from send_mail import send_mail
 if __name__ == '__main__':
     config = read_config('config.yaml')
 
-    checked = False
+    # lista wiadomości jakie przeszły przez sprawdzanie
+    # żeby nie słać co chwilę tego samego powiadomienia jeśli są ciągle nie przeczytanie wiadomości
+    known_msgs = {}
+    for u in config['librus_users']:
+        known_msgs[u['librus_login']] = []
+
+    # nieskończona pętla sprawdzania ciągle i wciąż
     while True:
-        try:
-            l = Librus(config)
-            l.get_messages()
-            checked = True
-        except Exception as e:
-            logger.error(f"Błąd w głównej pętli: {e}")
+        # dla każdego użytkownika przygotowujemy jego mini-konfigurację
+        for user_config in config['librus_users']:
             checked = False
 
-        if checked:
-            if l.unread_count:
-                logger.info("Pojawiły się nowe wiadomości")
-                send_mail(config, l.messages)
-            else:
-                logger.info("Brak nowych wiadomości")
+            # próba pobrania danych z Librusa
+            try:
+                l = Librus(user_config)
+                l.get_messages()
+                checked = True
+            except Exception as e:
+                logger.error(
+                    f"Błąd w głównej pętli dla {user_config['librus_login']}: {e}")
+                checked = False
 
-            del(l)
+            # czy udało się pobrać dane z Librusa?
+            if checked:
+                # aktualna lista wszystkich wiadomości
+                new_messages = [m['link'] for m in l.messages]
+                print(set(known_msgs[user_config['librus_login']]))
+                print(set(new_messages))
+                if set(known_msgs[user_config['librus_login']]) != set(new_messages):
+                    logger.info("Pojawiły się nowe wiadomości")
+
+                    # nadpisujemy listę znanych wiadomości
+                    known_msgs[user_config['librus_login']] = new_messages
+
+                    # wysyłamy mailowe powiadomienie
+                    send_mail(user_config, l.messages, new_messages)
+                else:
+                    logger.info("Brak nowych wiadomości")
+
+                del(l)
 
         logger.info(f"Czekam przez {config['wait_time_s']} sekund")
         sleep(config['wait_time_s'])
